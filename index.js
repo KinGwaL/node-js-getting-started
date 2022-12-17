0fa1527191e1b5d5fb887c78fdf1787a836db53d
King Wa Lai <king_lai@outlook.com>
2022-12-18 00:19:23 +0800

    update package

--- index.js
+++ index.js
@@ -1,112 +1,55 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
 const xPubrioKey = process.env.xPubrioKey;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const cluster = require("cluster");
+const totalCPUs = require("os").cpus().length;
 
-app.get('/', async (req, res) => {
-  var query = req.query.url;
-
-  if (!query || req.header('X-Pubrio-Key') != xPubrioKey) {
-    res.status(403).send({
-      status: 403,
-      code: '403', 
-      message: '403 Forbidden'
-    });
-    return;
+if (cluster.isMaster) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Master ${process.pid} is running`);
+ 
+  // Fork workers.
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
   }
+ 
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
 
-  try {
-    const options = { url: query, downloadLimit:5000000 };
-    const ogs = require('open-graph-scraper');
-    const data = await ogs(options);
-    res.send(data.result);
-  }catch (e) {
-    console.log(e);
-    res.sendStatus(501);
-  }
-  // try {
+  const app = express();
+  console.log(`Worker ${process.pid} started`);
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
+  app.get('/', async (req, res) => {
+    var query = req.query.url;
 
-  // //   .then((data) => {
-  // //     console.log(data);
-  // //     // const { error, result, response } = data;
-  // //     // console.log('error:', error);  // This returns true or false. True if there was an error. The error itself is inside the results object.
-  // //     // console.log('result:', result); // This contains all of the Open Graph results
-  // //     // console.log('response:', response); // This contains the HTML of page
-      
-  // //   }).catch(err => {
-  // //     console.log(err);
-  // //     res.sendStatus(501);
-  // // });
-  // }catch (e) {
-  //     console.log(e);
-  //     res.sendStatus(e);
-  // }
-  // const data = getContent(query)
-  // .then(metascraper)
-  // .then(metadata => console.log(metadata))
-  // .then(browserless.close)
-  // .then(process.exit)
-  // res.send(data);
-  //res.send(query);
-})
+    if (!query || req.header('X-Pubrio-Key') != xPubrioKey) {
+      res.status(403).send({
+        status: 403,
+        code: '403', 
+        message: '403 Forbidden'
+      });
+      return;
+    }
 
-// app.get('/api', async (req, res) => {
-//   if (req.header('X-Pubrio-Key') != xPubrioKey) {
-//     res.status(403).send({
-//       status: 403,
-//       code: '403', 
-//       message: '403 Forbidden'
-//     });
-//   }
+    try {
+      const options = { url: query, downloadLimit:5000000 };
+      const ogs = require('open-graph-scraper');
+      const data = await ogs(options);
+      res.send(data.result);
+    }catch (e) {
+      console.log(e);
+      res.sendStatus(501);
+    }
+  });
 
-//   var query = req.query.url;
-//   console.log(query);
-//   console.log(req);
-//   const options = { url: query };
-//   try {
+  app.listen(PORT);
 
-//     const getHTML = require('html-get')
-//     const browserless = require('browserless')()
-
-//     const getContent = async url => {
-//       // create a browser context inside the main Chromium process
-//       const browserContext = browserless.createContext()
-//       const promise = getHTML(url, { getBrowserless: () => browserContext })
-//       // close browser resources before return the result
-//       promise.then(() => browserContext).then(browser => browser.destroyContext())
-//       return promise
-//     }
-
-//     const metascraper = require('metascraper')([
-//       require('metascraper-author')(),
-//       require('metascraper-date')(),
-//       require('metascraper-description')(),
-//       require('metascraper-image')(),
-//       require('metascraper-logo')(),
-//       require('metascraper-clearbit')(),
-//       require('metascraper-publisher')(),
-//       require('metascraper-title')(),
-//       require('metascraper-url')()
-//     ])
-
-//   getContent(query)
-//   .then(metascraper)
-//   .then(metadata => res.send(metadata))
-//   .then(browserless.close)
-//   .then(process.exit)
-
-//   }catch (e) {
-//       console.log(e);
-//       res.sendStatus(e);
-//   }
-
-//   //res.send(query);
-// })
-
-
-
-app.listen(PORT);
+}
